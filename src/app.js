bbf3d3f9d4e8aaca6fa00c215e23e7349f540dce
kadirkiras <kadir_piece@outlook.com>
2021-10-19 00:13:16 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,11 @@
+console.log('Merhaba dunya');
+
+let konutKredileri = ["Konut kredisi","Emlak konut Kredisi","Kamu Konut Kredisi","Tasit Kredisi"];
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>") 
+    
+}
+console.log("</ul>")
+
